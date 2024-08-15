d93f69b942ffe8c3c2b42445a4f30e67283f11dc
Lucas Brito <103859561+kikkask@users.noreply.github.com>
2024-08-15 11:24:10 -0300

    Create Loteria.py
    Programa para adivinhar o número da sorte com função

---
+++ Loteria.py
@@ -0,0 +1,32 @@
+def check_input():
+  try:
+    return int(input("Entre com um número entre 1 e 15: "))
+  except ValueError:
+    return "Eu pedi um número, fiote"
+
+def check_interval(numero):
+  return 1 <= numero <= 15
+
+def valida_entrada():
+  while True:
+    numero = check_input()
+if type(numero) != int:
+  print(numero)
+  continue
+if check_interval(numero):
+  return numero
+
+numero_sorte = 7
+
+for i in range(3):
+
+  numero = valida_entrada()
+
+  if numero == numero_sorte:
+    print("Você acertou!!")
+    break
+
+  elif numero > numero_sorte:
+    print("Você errou!! Tente um número menor!")
+  else:
+    print("Você errou!! Tente um número maior!")
